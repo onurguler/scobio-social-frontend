98187df010838d3b134fc8f7cdae8ba301b94d7d
Onur Güler <guleronurosman@gmail.com>
2019-11-29 19:33:11 +0300

    update profile

---
+++ client/src/components/profile/NewScob.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPlusSquare } from '@fortawesome/free-solid-svg-icons';
+
+const NewScob = () => {
+  return (
+    <div className="mb-4 border rounded-lg px-4 py-4 bg-white profile-post shadow-sm">
+      <div className="d-flex mb-2 align-items-center w-100">
+        <img
+          className="rounded-circle fit-image"
+          src="https://scontent-otp1-1.xx.fbcdn.net/v/t1.0-9/p960x960/65123467_10219910636463233_5371741452810321920_o.jpg?_nc_cat=102&_nc_oc=AQllQE9F2oHnz03qbVXp8zJ2yRunR6djtQEc5PDdaZqjBgLbED-_WZqdI-tfvCMpPc4&_nc_ht=scontent-otp1-1.xx&oh=360ed3d04eab2e7462dd54d33479b6e0&oe=5E5343CC"
+          width="50"
+          height="50"
+          alt=""
+        />
+        <div className="ml-3">
+          <div className="font-weight-bold">Ege Çakmak</div>
+        </div>
+      </div>
+      <div class="form-group shadow-textarea profile-post w-100">
+        <textarea
+          class="form-control z-depth-1"
+          id="exampleFormControlTextarea6"
+          rows="3"
+          placeholder="Buraya bir şeyler yazın..."></textarea>
+      </div>
+      <button className="btn btn-outline-blue-500 profile-post w-100">
+        <FontAwesomeIcon icon={faPlusSquare} className="mr-2" />
+        Scob
+      </button>
+    </div>
+  );
+};
+
+export default NewScob;
--- client/src/components/profile/ProfileNav.js
+++ client/src/components/profile/ProfileNav.js
@@ -1,4 +1,7 @@
 import React, { useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faAlignJustify } from '@fortawesome/free-solid-svg-icons';
+import { faClock, faBookmark } from '@fortawesome/free-regular-svg-icons';
 
 function ProfileNav({ setShowPosts, setShowScobs, setShowBookmarks }) {
   const [postsIsActive, setPostsIsActive] = useState(true);
@@ -6,53 +9,107 @@ function ProfileNav({ setShowPosts, setShowScobs, setShowBookmarks }) {
   const [bookmarksIsActive, setBookmarksIsActive] = useState(false);
 
   return (
-    <div>
-      <nav class="nav nav-pills flex-column flex-sm-row mb-3">
-        <a
-          class={`flex-sm-fill text-sm-center nav-link ${postsIsActive &&
-            'active'}`}
-          href="#"
-          onClick={() => {
-            setPostsIsActive(true);
-            setScobsIsActive(false);
-            setBookmarksIsActive(false);
-            setShowPosts(true);
-            setShowScobs(false);
-            setShowBookmarks(false);
-          }}>
-          Posts
-        </a>
-        <a
-          class={`flex-sm-fill text-sm-center nav-link ${scobsIsActive &&
-            'active'}`}
-          href="#"
-          onClick={() => {
-            setPostsIsActive(false);
-            setScobsIsActive(true);
-            setBookmarksIsActive(false);
-            setShowPosts(false);
-            setShowScobs(true);
-            setShowBookmarks(false);
-          }}>
-          Scobs
-        </a>
-        <a
-          class={`flex-sm-fill text-sm-center nav-link ${bookmarksIsActive &&
-            'active'}`}
-          href="#"
-          onClick={() => {
-            setPostsIsActive(false);
-            setScobsIsActive(false);
-            setBookmarksIsActive(true);
-            setShowPosts(false);
-            setShowScobs(false);
-            setShowBookmarks(true);
-          }}>
-          Bookmarks
-        </a>
-      </nav>
+    <div className="d-flex mb-4 border-bottom pb-2 bg-white profile-nav">
+      <div className="d-flex w-100 align-items-center justify-content-between">
+        <div className="w-100">
+          <button
+            className={`btn btn-default text-center text-gray-600 px-4 w-100 font-semibold text-uppercase btn-sm ${postsIsActive &&
+              'active'}`}
+            onClick={() => {
+              setPostsIsActive(true);
+              setScobsIsActive(false);
+              setBookmarksIsActive(false);
+              setShowPosts(true);
+              setShowScobs(false);
+              setShowBookmarks(false);
+            }}>
+            <FontAwesomeIcon className="text-center" icon={faAlignJustify} />
+            <br />
+            <span>Posts</span>
+          </button>
+        </div>
+        <div className="w-100">
+          <button
+            className={`btn btn-default text-center text-gray-600 px-4 w-100 font-semibold text-uppercase btn-sm ${scobsIsActive &&
+              'active'}`}
+            onClick={() => {
+              setPostsIsActive(false);
+              setScobsIsActive(true);
+              setBookmarksIsActive(false);
+              setShowPosts(false);
+              setShowScobs(true);
+              setShowBookmarks(false);
+            }}>
+            <FontAwesomeIcon className="text-center" icon={faClock} />
+            <br />
+            <span>Scobs</span>
+          </button>
+        </div>
+        <div className="w-100">
+          <button
+            className={`btn btn-default text-center text-gray-600 px-4 w-100 font-semibold text-uppercase btn-sm ${bookmarksIsActive &&
+              'active'}`}
+            onClick={() => {
+              setPostsIsActive(false);
+              setScobsIsActive(false);
+              setBookmarksIsActive(true);
+              setShowPosts(false);
+              setShowScobs(false);
+              setShowBookmarks(true);
+            }}>
+            <FontAwesomeIcon className="text-center" icon={faBookmark} />
+            <br />
+            <span>Bookmarks</span>
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
 
+// return (
+//   <div className="profile-post">
+//     <nav class="nav nav-pills nav-fill nav-justified mb-3">
+//       <a
+//         class={`nav-item nav-link ${postsIsActive && 'active'}`}
+//         href="#"
+//         onClick={() => {
+//           setPostsIsActive(true);
+//           setScobsIsActive(false);
+//           setBookmarksIsActive(false);
+//           setShowPosts(true);
+//           setShowScobs(false);
+//           setShowBookmarks(false);
+//         }}>
+//         Posts
+//       </a>
+//       <a
+//         class={`nav-item nav-link ${scobsIsActive && 'active'}`}
+//         href="#"
+// onClick={() => {
+//   setPostsIsActive(false);
+//   setScobsIsActive(true);
+//   setBookmarksIsActive(false);
+//   setShowPosts(false);
+//   setShowScobs(true);
+//   setShowBookmarks(false);
+// }}>
+//         Scobs
+//       </a>
+//       <a
+//         class={`nav-item nav-link ${bookmarksIsActive && 'active'}`}
+//         href="#"
+// onClick={() => {
+//   setPostsIsActive(false);
+//   setScobsIsActive(false);
+//   setBookmarksIsActive(true);
+//   setShowPosts(false);
+//   setShowScobs(false);
+//   setShowBookmarks(true);
+// }}>
+//         Bookmarks
+//       </a>
+//     </nav>
+//   </div>
+// );
 export default ProfileNav;
